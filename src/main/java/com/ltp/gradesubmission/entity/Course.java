58db6997f113349eabde9927d7d898ee8e86788d
Srishti Ahirwar <ahirwar.s@northeastern.edu>
2024-06-21 13:01:15 -0400

    first commit

---
+++ src/main/java/com/ltp/gradesubmission/entity/Course.java
@@ -0,0 +1,44 @@
+package com.ltp.gradesubmission.entity;
+
+import java.util.List;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank(message="Subject cannot be blank")
+    @NonNull
+    @Column(name = "subject", nullable = false)
+    private String subject;
+
+    @NotBlank(message="Course Code cannot be blank")
+    @NonNull
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+    @NotBlank(message="Description cannot be blank")
+    @NonNull
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<Grade> grades;
+
+}
